098fa5c64c8b18ba0c98465b34d66a1ff9a2aa88
Meiying Hu <meiyinghu18@gmail.com>
2025-02-16 16:58:00 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import React, { useState, useEffect } from "react";
 import CardBox from "./components/CardBox";
 import Footer from "./components/Footer";
 import TopBar from "./components/TopBar";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="bg-light">
+    <div className="d-flex flex-column min-vh-100">
       <TopBar />
-      <CardBox />
+      <div className="flex-grow-1">
+        {isLoading ? (
+          <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "300px" }}>
+            <CircularProgress />
+          </div>
+        ) : (
+          <CardBox />
+        )}
+      </div>
       <Footer />
     </div>
   );
